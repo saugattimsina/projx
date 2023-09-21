31947654201d7c8d2cef59c32b9894259ae743f8
aadarsha000 <aadarshakarki000@gmail.com>
2023-09-21 21:15:30 +0545

    feat: eddited user model to add qr image for 2fa authenticator app eddited login for 2fa created otp verify api

--- accountsapi/urls.py
+++ accountsapi/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserLoginApiView, UserRegistrationApiView
+from .views import UserLoginApiView, UserRegistrationApiView, VerityOTPView
 
 
 urlpatterns = [
     # user Api
     path("login/", UserLoginApiView.as_view()),
     path("registrations/", UserRegistrationApiView.as_view()),
+    path("verify/otp/", VerityOTPView.as_view()),
 ]
