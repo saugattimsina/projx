f1bac5d5dbff7aa86c8bf30a1ac84ebd8cce22d4
saugattimsina <saugattimsina97@gmail.com>
2023-10-09 14:05:40 +0545

    added the subscription oinly for user

--- subscription/signals.py
+++ subscription/signals.py
@@ -71,8 +71,8 @@ be added tooo first need to finish the binary model
 @receiver(post_save, sender=User)
 def create_default_subscription(sender, instance, created, **kwargs):
     if created:
-        default_subscription = Subscription.objects.get(package_type='free')
         if not instance.is_superuser:
+            default_subscription = Subscription.objects.get(package_type='free')
             if default_subscription:
                 UserSubcription.objects.create(user=instance, plan=default_subscription)
 
