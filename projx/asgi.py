f6441fc1feac68b9d867c1c8e9f4f03eaa23db8d
Aadarsha Karki <aadarshakarki000@gmail.com>
2024-05-08 18:18:24 +0545

    changed asgi

--- projx/asgi.py
+++ projx/asgi.py
@@ -1,23 +1,16 @@
-"""
-ASGI config for projx project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projx.settings")
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projx.settings")
-
-django_asgi_app = get_asgi_application()
 
 from .token_auth import TokenAuthMiddleware
 from signalbot.routing import websocket_urlpatterns
+
+django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter(
     {
